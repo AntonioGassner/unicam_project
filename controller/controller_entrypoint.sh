6164268ab4f5728ebce8a9064ea030e4c4dba760
AntonioGassner <56674343+AntonioGassner@users.noreply.github.com>
2022-02-21 18:22:30 +0100

    v5

---
+++ controller/controller_entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/robomaker/project_ccc
+source /opt/ros/foxy/setup.bash
+source /usr/share/gazebo-11/setup.sh
+source ./install/setup.sh
+export SVGA_VGPU10=0
+source /usr/share/colcon_cd/function/colcon_cd.sh
+export _colcon_cd_root=~/ros2_install
+export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11
+sudo chmod -R 777 /home/robomaker
+printenv
+
+exec "${@:1}"
