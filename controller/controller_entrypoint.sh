a75684cf065a01e82494852446613e083b312b4e
AntonioGassner <56674343+AntonioGassner@users.noreply.github.com>
2022-03-07 23:01:23 +0100

    logging kinda working

--- controller/controller_entrypoint.sh
+++ controller/controller_entrypoint.sh
@@ -7,7 +7,5 @@ export SVGA_VGPU10=0
 source /usr/share/colcon_cd/function/colcon_cd.sh
 export _colcon_cd_root=~/ros2_install
 export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11
-export ROS_LOG_DIR=/home/robomaker/my_logs
-gzserver -r --record path /home/robomaker/my_logs /home/robomaker/project_ccc/src/ccc_pkg/worlds/empty.world --record_encoding txt
 
 exec "${@:1}"
