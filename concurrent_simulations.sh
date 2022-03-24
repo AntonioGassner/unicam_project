037a7509c2d3a0272ebe99be06c3493a3a647dc8
AntonioGassner <56674343+AntonioGassner@users.noreply.github.com>
2022-03-24 15:17:43 +0100

    added separate networks to fix overwriting issue

--- concurrent_simulations.sh
+++ concurrent_simulations.sh
@@ -1,20 +1,31 @@
 #! /bin/bash
 #set n to the number of simulations you want in total.
-n=2
+n=10
 
 #simple while loop to create simulations in subshell
 i=1
 port1=5555
 port2=11311
+network_id=1
 docker volume prune -f
+docker network prune -f
+
+while [ "$network_id" -le "$n" ]
+do 
+	docker network create network_"$network_id"
+	network_id=$(("network_id"+1))
+done
+
+network_id=1
 
 while [ "$i" -le "$n" ]
 do 
-	(trap 'kill 0' SIGINT; docker run -d --rm -v simulation_log_"$i":/home/robomaker/my_logs --name sim_"$i" -u robomaker -e ROBOMAKER_GAZEBO_MASTER_URI=http://localhost:"$port1" -e ROBOMAKER_ROS_MASTER_URI=http://localhost:"$port2" simulation ) &
-	(trap 'kill 0' SIGINT; docker run -d --rm --name cont_"$i" -u robomaker -e ROBOMAKER_GAZEBO_MASTER_URI=http://localhost:"$port1" -e ROBOMAKER_ROS_MASTER_URI=http://localhost:"$port2" controller )
+	(trap 'kill 0' SIGINT; docker run -d --network network_"$network_id" --rm -v simulation_log_"$i":/home/robomaker/my_logs --name sim_"$i" -u robomaker -e ROBOMAKER_GAZEBO_MASTER_URI=http://localhost:"$port1" -e ROBOMAKER_ROS_MASTER_URI=http://localhost:"$port2" simulation ) &
+	(trap 'kill 0' SIGINT; docker run -d --network network_"$network_id" --rm --name cont_"$i" -u robomaker -e ROBOMAKER_GAZEBO_MASTER_URI=http://localhost:"$port1" -e ROBOMAKER_ROS_MASTER_URI=http://localhost:"$port2" controller )
 	port1=$(("port1"+1))
 	port2=$(("port2"+1))
 	i=$(("i"+1))
+	network_id=$(("network_id"+1))
 done
 
 while true
@@ -28,4 +39,6 @@ else
     break
 fi 
 done
+
+
 
