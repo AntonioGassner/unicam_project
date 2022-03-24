12aee9b6058bb6d57b1836885acdfc3255617a66
AntonioGassner <56674343+AntonioGassner@users.noreply.github.com>
2022-03-24 15:15:57 +0100

    some cleanup + ros topic logging

--- controller/controller_entrypoint.sh
+++ controller/controller_entrypoint.sh
@@ -7,5 +7,7 @@ export SVGA_VGPU10=0
 source /usr/share/colcon_cd/function/colcon_cd.sh
 export _colcon_cd_root=~/ros2_install
 export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11
+cd /home/robomaker/my_logs
+ros2 bag record -o subset /dolly_1/cmd_vel &
 
 exec "${@:1}"
