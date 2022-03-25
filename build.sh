7044c29d141710e704c3f4686d43bf15ebc50002
AntonioGassner <56674343+AntonioGassner@users.noreply.github.com>
2022-03-25 16:51:22 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+#commands to build all images relative to our project, change directories accordingly if your file structure is different for some reason
 sudo docker pull ros:foxy-ros-core-focal
 cd ~/unicam_project/base_image
+# -t is used to tag the image, the base_image after it is the name. change at your own risk, since the subsequent images use the base_image
 sudo docker build -t base_image .
 cd ..
 cd simulation
