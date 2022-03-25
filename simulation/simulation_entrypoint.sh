c2964c4ba6f838d4805844aba03367e86bb7e3b1
AntonioGassner <56674343+AntonioGassner@users.noreply.github.com>
2022-03-25 16:53:30 +0100

    Update simulation_entrypoint.sh

--- simulation/simulation_entrypoint.sh
+++ simulation/simulation_entrypoint.sh
@@ -7,6 +7,9 @@ export SVGA_VGPU10=0
 source /usr/share/colcon_cd/function/colcon_cd.sh
 export _colcon_cd_root=~/ros2_install
 export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11
+
+
+#these commands are to record ros2 topics, where /dolly/cmd_vel is the name of the topic in this instance
 cd /home/robomaker/my_logs
 ros2 bag record -o subset /dolly_1/cmd_vel &
 
